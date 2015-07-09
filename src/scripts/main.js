b87a47a4199f47fa54d1c4c93851d93733ecda18
Andrea Tosatto <andrea.tosy@gmail.com>
2015-07-09 17:06:14 +0200

    Are we ready?! Never

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -89,3 +89,14 @@ var deck = bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
+
+// Docker Language support
+Prism.languages.docker = {
+	'keyword': {
+		pattern: /(^\s*)(?:ONBUILD|FROM|MAINTAINER|RUN|EXPOSE|ENV|ADD|COPY|VOLUME|USER|WORKDIR|CMD|LABEL|ENTRYPOINT)(?=\s)/mi,
+		lookbehind: true
+	},
+	'string': /("|')(\\\n|\\?.)*?\1/,
+	'comment': /#.*/,
+	'punctuation': /([:[\]{}\-,|>?]|---|\.\.\.)/
+};
